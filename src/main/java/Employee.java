c237eef884b52e8d9731f2f977a434d1befebd30
soujangadi <ts-soujanya.angadi@10.99.63.4>
2023-05-25 15:46:10 +0530

    new project Spring Maven

---
+++ src/main/java/Employee.java
@@ -0,0 +1,12 @@
+
+public class Employee {
+	private String name;
+	public Employee(String name) {
+		this.name=name;
+	}
+
+	public void displayname() {
+		System.out.println("Welecome to Maven :"+this.name);
+	}
+
+}
---
+++ src/main/java/TestEmployee.java
@@ -0,0 +1,16 @@
+
+	import org.springframework.context.ApplicationContext;
+	import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+	public class TestEmployee {
+	public static void main(String[] args) {
+			
+			ApplicationContext context= new ClassPathXmlApplicationContext("EmployeeContext.xml");
+			Employee emp=(Employee)context.getBean("empbean");
+		    emp.displayname();
+
+	}
+	}
+
+
+
